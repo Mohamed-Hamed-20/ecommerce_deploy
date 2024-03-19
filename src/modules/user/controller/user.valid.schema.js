f4ab973f051763d527322f30b07c7427e3e9c7b2
Mohamed-Hamed-20 <125780715+Mohamed-Hamed-20@users.noreply.github.com>
2024-03-19 20:03:52 +0200

    DEV

--- src/modules/user/controller/user.valid.schema.js
+++ src/modules/user/controller/user.valid.schema.js
@@ -1,40 +1,64 @@
-import joi from 'joi'
+import joi from "joi";
+import {
+  customMessages,
+  generalFields,
+} from "../../../middleware/validation.js";
 export const singup = {
   body: joi
-        .object({
-      firstName : joi.string().min(3).max(10),
-      lastName : joi.string().min(3).max(10),
-      userName: joi.string().min(3).max(20).required(),
-      email : joi.string().email({tlds:{allow:["com", "net","gov"]}}).required(),
-      password:joi.string().regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/),
-      cpassword: joi.string().valid(joi.ref("password")).required(),
-      gender: joi.string().valid("male", "female"),
+    .object({
+      firstName: joi.string().min(3).max(10).messages(customMessages),
+      lastName: joi.string().min(3).max(10).messages(customMessages),
+      userName: joi.string().min(3).max(20).required().messages(customMessages),
+      email: generalFields.email.required().messages(customMessages),
+      password: generalFields.password.required().messages(customMessages),
+      cpassword: joi
+        .string()
+        .valid(joi.ref("password"))
+        .required()
+        .messages(customMessages),
+      gender: joi.string().valid("male", "female").messages(customMessages),
     })
-    .required(),
+    .required()
+    .messages(customMessages),
   // paramas: joi.object().required(),
   // query: joi.object().required(),
   // file: joi.object().required(),
 };
 
 export const login = {
-  body:joi.object({
-      email : joi.string().email({tlds:{allow:["com", "net","gov"]}}).required(),
-      password:joi.string().regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/),
-  }).required(),
+  body: joi
+    .object({
+      email: generalFields.email.required().messages(customMessages),
+      password: generalFields.password.required().messages(customMessages),
+    })
+    .required(),
 };
 
-export const forgetPass ={
-  body:joi.object({
-    email:joi.string().email().required(),
-  }).required(),
-}
+export const forgetPass = {
+  body: joi
+    .object({
+      email: generalFields.email.required().messages(customMessages),
+    })
+    .required(),
+};
 
 export const resetpassword = {
-  body:joi.object({
-    email: joi.string().email().required(),
-    password:joi.string().regex(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/).required(),
-    confirmPassword:joi.string().valid(joi.ref("password")).required(),
-    forgetCode:joi.string().min(7).max(7).required(),
-  
-  }).required(),
-}
+  body: joi
+    .object({
+      email: generalFields.email.required().messages(customMessages),
+      password: generalFields.password.required().messages(customMessages),
+      confirmPassword: joi
+        .string()
+        .valid(joi.ref("password"))
+        .required()
+        .messages(customMessages),
+      forgetCode: joi
+        .string()
+        .min(7)
+        .max(7)
+        .required()
+        .messages(customMessages),
+    })
+    .required()
+    .messages(customMessages),
+};
